cab84cc39bb33951bd34c4b50cd3f37b704006c0
Raay109 <156700408+Raay109@users.noreply.github.com>
2024-11-06 14:50:41 +0000

    upd

--- ultimate/src/components/Navbar/Navbar.jsx
+++ ultimate/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,33 @@
 import React, { act } from 'react'
 import "./Navbar.css"
-
+let activ = false;
+let root = 0;
+let root2 = 0;
 function Navbar(){
 
 
 function slidebar() {
-  let activ = false;
-
+ const line1 = document.querySelector(`.nav-line-1`);
+ const line2 = document.querySelector(`.nav-line-2`);
   const slidebar = document.querySelector(".slidebar");
   if (activ == false){
     activ = true;
+    root -= 45;
+    root2 = 45;
+    line1.style.transform = `rotate(${root}deg)`;
+    line2.style.transform = `rotate(${root2}deg)`;
     slidebar.style.width = '100%';
+    line1.style.top = 7 + `px`;
+    line2.style.top = 0 + `px`;
   }else {
+    activ = false;
 slidebar.style.width = '0%';
+root = 0;
+root2 = 0;
+line1.style.top = 0 + `px`;
+line2.style.top = 0 + `px`;
+line1.style.transform = `rotate(${root}deg)`;
+line2.style.transform = `rotate(${root2}deg)`;
   }
 }
   return (
@@ -24,6 +39,7 @@ slidebar.style.width = '0%';
         <div class="nav-line-2"> </div>
     </nav>
 </nav>
+
     </div>
   )
 }
