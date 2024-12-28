8566208c46beaf252937c3d05d3c2f907b9d13dd
Raay109 <156700408+Raay109@users.noreply.github.com>
2024-12-28 21:12:50 +0000

    update

--- terament/src/App.jsx
+++ terament/src/App.jsx
@@ -3,7 +3,11 @@ import Navbar from './components/Navbar/Navbar'
 function App() {
   return (
     <div>
+      {window.innerWidth > 200 && (
+        <div>
       <Navbar/>
+      </div>
+)}
     </div>
   )
 }
